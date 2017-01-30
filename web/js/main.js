85c7496b5e6334bcd153c7579dac7a17e4eb21f4
Vladimir Bespechny <yankeeeeeman@gmail.com>
2017-01-30 19:48:09 +0200

    [+] end Desktop version [+] googleMap

--- web/js/main.js
+++ web/js/main.js
@@ -67,6 +67,66 @@ var AppJS = {
         });
     },
 
+    initMap: function () {
+        var mapBox = $('.map');
+        var center = {lat: 40.703017, lng: -73.987023};
+        var markerIcon = 'img/icon/markerIcon.svg';
+
+        var styleArray = [
+            {
+                featureType: "all",
+                stylers: [
+                    { saturation: -100 }
+                ]
+            },{
+                featureType: "road.arterial",
+                elementType: "geometry",
+                stylers: [
+                    { hue: "#00ffee" },
+                    { saturation: 100 }
+                ]
+            },{
+                featureType: "road.local",
+                elementType: "labels",
+                stylers: [
+                    {visibility: 'off'}
+                ]
+            },{
+                featureType: "poi.business",
+                elementType: "labels",
+                stylers: [
+                    { visibility: "off" }
+                ]
+            }
+        ];
+
+        var styledMap = new google.maps.StyledMapType(styleArray, {name: "Styled Map"});
+
+        var map = new google.maps.Map(mapBox[0], {
+            center: center,
+            scrollwheel: false,
+            zoom: 13
+        });
+
+        var image = new google.maps.MarkerImage(
+            markerIcon,
+            new google.maps.Size(50, 50),
+            new google.maps.Point(0, 0),
+            new google.maps.Point(0, 25),
+            new google.maps.Size(50, 50)
+        );
+
+        var marker = new google.maps.Marker({
+            position: center,
+            map: map,
+            title: 'Hello World!',
+            icon: image,
+        });
+
+        map.mapTypes.set('map_style', styledMap);
+        map.setMapTypeId('map_style');
+    },
+
     pageAnimate: function () {
         var scroll = $('body').scrollTop();
         var winH = window.innerWidth;
@@ -78,6 +138,9 @@ var AppJS = {
         var s6 = $('.s6');
         var s8 = $('.s8');
         var s9 = $('.s9');
+        var s10 = $('.s10');
+        var s11 = $('.s11');
+        var s12 = $('.s12');
 
         var s2Top = s2.position().top;
         var s3Top = s3.position().top;
@@ -85,11 +148,14 @@ var AppJS = {
         var s6Top = s6.position().top;
         var s8Top = s8.position().top;
         var s9Top = s9.position().top;
+        var s10Top = s10.position().top;
+        var s11Top = s11.position().top;
+        var s12Top = s12.position().top;
 
         if (scroll < 20) {
             header.removeClass('headerBack');
         }
-
+        
         if (scroll >= 0) {
             header.add(s1).addClass('animate');
         }
@@ -113,6 +179,15 @@ var AppJS = {
         }
         if (winH / 2 >= s9Top - scroll) {
             s9.addClass('animate');
+        }
+        if (winH / 2 >= s10Top - scroll) {
+            s10.addClass('animate');
+        }
+        if (winH / 2 >= s11Top - scroll) {
+            s11.addClass('animate');
+        }
+        if (winH / 2 >= s12Top - scroll) {
+            s12.addClass('animate');
         }
     },
 
